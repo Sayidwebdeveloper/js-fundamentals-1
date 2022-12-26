71e705d408c01672e56e774c819d1425f26a6110
Sayidwebdeveloper <sayidfuad4027@gmail.com>
2022-12-26 22:24:52 +0300

    my first activity

--- exercise.js
+++ exercise.js
@@ -1,2 +1,48 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let country = "Somaliland"
+
+let continent ="Africa"
+let language ="Af-somali"
+let oncoast = "Berbera beach"
+
+let i = 4000000;
+
+console.log("Somaliland");
+
+console.log("Africa");
+
+console.log(i);
+
+ console.log("Af-somali");
+
+ console.log("berbera beach");
+
+i +=1000000
+console.log(i)
+// Q7
+let var1 =5000000;
+let var2  = 6000000;
+console.log( 5000000 == var2 )
+
+ //If & Else statements
+
+ // Q9
+ let  describtion = "continent, country , population";
+
+
+// Q8 and Q10
+let Somaliland = 5000000;
+let  x = 33000000;
+console.log( Somaliland < x)
+
+if (Somaliland < x)
+console.log(" YourCountry's population is YourPopulation, bellow average")
+
+// Q11 Logical Operators
+
+let Ahmed = "looking for new country 50 million";
+console.log("You Couldn't Live in My Country");
+
